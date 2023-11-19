2a3c61db8fc284b88dfd548cd658b4e44d7de177
Maria <73954652+garmashmaha@users.noreply.github.com>
2023-11-19 16:01:39 +0000

    Edit 19.11

--- lab1_code/zadania.py
+++ lab1_code/zadania.py
@@ -1,28 +1,11 @@
 import json
 import time
 
-# Wczytaj dane z pliku JSON
-with open("imiona.json", "r") as file:
-    lista_imion = json.load(file)
-
-# Wyświetl listę najpopularniejszych imion dzieci
-print(lista_imion)
-
 def suma_od_1_do_n(n):
     suma = 0
     for i in range(1, n + 1):
         suma += i
     return suma
-
-# Odczytaj wartość n z klawiatury
-n = int(input("Podaj wartość n: "))
-
-start_time = time.time()
-wynik = suma_od_1_do_n(n)
-end_time = time.time()
-
-print("Wynik:", wynik)
-print("Czas wykonania:", end_time - start_time, "sekundy")
 
 def znajdz_imie(imie, lista_imion):
     if imie in lista_imion:
@@ -30,6 +13,17 @@ def znajdz_imie(imie, lista_imion):
     else:
         return f"Imię {imie} nie występuje na liście."
 
+def znajdz_imie_w_typie(imie, lista_imion, typ_imienia):
+    if imie in [elem['name'] for elem in lista_imion if elem['type'] == typ_imienia]:
+        return f"Imię {imie} jest na liście typu {typ_imienia}."
+    else:
+        return f"Imię {imie} nie występuje na liście typu {typ_imienia}."
+
+# ZADANIE 2: Wczytaj dane z pliku JSON
+with open("imiona.json", "r") as file:
+    lista_imion = json.load(file)
+
+# ZADANIE 3: Wyszukiwanie imienia "Julian" w liście imion
 imie_do_wyszukania = "Julian"
 
 start_time = time.time()
@@ -39,12 +33,7 @@ end_time = time.time()
 print(wynik)
 print("Czas wykonania:", end_time - start_time, "sekundy")
 
-def znajdz_imie_w_typie(imie, lista_imion, typ_imienia):
-    if imie in [elem['name'] for elem in lista_imion if elem['type'] == typ_imienia]:
-        return f"Imię {imie} jest na liście typu {typ_imienia}."
-    else:
-        return f"Imię {imie} nie występuje na liście typu {typ_imienia}."
-
+# ZADANIE 4: Wyszukiwanie imienia "Julian" w liście imion typu "HISPANIC"
 imie_do_wyszukania = "Julian"
 typ_imienia_do_wyszukania = "HISPANIC"
 
@@ -55,6 +44,7 @@ end_time = time.time()
 print(wynik)
 print("Czas wykonania:", end_time - start_time, "sekundy")
 
+# ZADANIE 5: Tworzenie unikalnej listy imion bez powtórzeń
 unikalne_imiona = list(set([elem['name'] for elem in lista_imion]))
 
 # Wypisz unikalne imiona
