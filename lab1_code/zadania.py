34356740d525466bd831f7a25d00f748670832ca
Maria <73954652+garmashmaha@users.noreply.github.com>
2023-11-19 16:35:58 +0000

    EDIT

--- lab1_code/zadania.py
+++ lab1_code/zadania.py
@@ -2,50 +2,45 @@ import json
 import time
 
 def suma_od_1_do_n(n):
-    suma = 0
-    for i in range(1, n + 1):
-        suma += i
-    return suma
+    return sum(range(1, n + 1))
+
+def zmierz_czas(funkcja, *args):
+    start_time = time.time()
+    wynik = funkcja(*args)
+    end_time = time.time()
+    return wynik, end_time - start_time
+
+def wczytaj_dane_z_json(sciezka):
+    with open(sciezka, "r") as file:
+        return json.load(file)
 
 def znajdz_imie(imie, lista_imion):
-    if imie in lista_imion:
-        return f"Imię {imie} jest na liście."
-    else:
-        return f"Imię {imie} nie występuje na liście."
+    return f"Imię {imie} {'jest' if imie in lista_imion else 'nie występuje'} na liście."
 
 def znajdz_imie_w_typie(imie, lista_imion, typ_imienia):
-    if any(elem['name'] == imie and elem['type'] == typ_imienia for elem in lista_imion):
-        return f"Imię {imie} jest na liście typu {typ_imienia}."
-    else:
-        return f"Imię {imie} nie występuje na liście typu {typ_imienia}."
+    is_imie = any(elem['name'] == imie and elem['type'] == typ_imienia for elem in lista_imion)
+    return f"Imię {imie} {'jest' if is_imie else 'nie występuje'} na liście typu {typ_imienia}."
 
 # ZADANIE 2: Wczytaj dane z pliku JSON
-with open("/home/codespace/.python/garmashmaha/lab1_code/imiona.json", "r") as file:
-    lista_imion = json.load(file)
+sciezka_do_pliku = "/home/codespace/.python/garmashmaha/lab1_code/imiona.json"
+lista_imion = wczytaj_dane_z_json(sciezka_do_pliku)
 
 # ZADANIE 3: Wyszukiwanie imienia "Julian" w liście imion
 imie_do_wyszukania = "Julian"
-
-start_time = time.time()
-wynik = znajdz_imie(imie_do_wyszukania, [elem['name'] for elem in lista_imion])
-end_time = time.time()
+wynik, czas_wyszukania = zmierz_czas(znajdz_imie, imie_do_wyszukania, [elem['name'] for elem in lista_imion])
 
 print(wynik)
-print("Czas wykonania:", end_time - start_time, "sekundy")
+print("Czas wykonania:", czas_wyszukania, "sekundy")
 
 # ZADANIE 4: Wyszukiwanie imienia "Julian" w liście imion typu "HISPANIC"
-imie_do_wyszukania = "Julian"
 typ_imienia_do_wyszukania = "HISPANIC"
-
-start_time = time.time()
-wynik = znajdz_imie_w_typie(imie_do_wyszukania, lista_imion, typ_imienia_do_wyszukania)
-end_time = time.time()
+wynik, czas_wyszukania_typ = zmierz_czas(znajdz_imie_w_typie, imie_do_wyszukania, lista_imion, typ_imienia_do_wyszukania)
 
 print(wynik)
-print("Czas wykonania:", end_time - start_time, "sekundy")
+print("Czas wykonania:", czas_wyszukania_typ, "sekundy")
 
 # ZADANIE 5: Tworzenie unikalnej listy imion bez powtórzeń
-unikalne_imiona = list(set(elem['name'] for elem in lista_imion))
+unikalne_imiona = list({elem['name'] for elem in lista_imion})
 
 # Wypisz unikalne imiona
 print(unikalne_imiona)
