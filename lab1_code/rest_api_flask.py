4a61cf3d6274f3cdcb7d72c35bf02a9c48ef8f3d
Maria <73954652+garmashmaha@users.noreply.github.com>
2023-12-13 08:40:54 +0100

    6-10 rest_api_flask.py

--- lab1_code/rest_api_flask.py
+++ lab1_code/rest_api_flask.py
@@ -33,3 +33,36 @@ def znajdz_imie_w_typie(imie, lista_imion, typ_imienia):
 # ZADANIE5: Tworzenie unikalnej listy imion bez powtórzeń
 def unikalne_imiona(lista_imion):
     return list({elem['name'] for elem in lista_imion})
+
+# ZADANIE6: Generowanie tablicy 100 pseudo-randomowych liczb
+def generuj_liczby():
+    return [random.randint(1, 1000) for _ in range(100)]
+
+# ZADANIE7: Implementacja sortowania bąbelkowego
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# ZADANIE8: Sortowanie setu z ZADANIA4 za pomocą sortowania bąbelkowego
+def sortuj_set(set_do_posortowania):
+    posortowany_set = list(set_do_posortowania)
+    return bubble_sort(posortowany_set)
+
+# ZADANIE9: Wyszukiwanie imienia 'JULIAN' w zbiorze/liście z ZADANIA7
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == item:
+            return True
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
