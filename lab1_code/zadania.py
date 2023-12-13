f864d5d1da8b46bd5a28672a3102d0f3536740e8
Maria <73954652+garmashmaha@users.noreply.github.com>
2023-12-13 08:34:40 +0100

    6-10 zadania.py

--- lab1_code/zadania.py
+++ lab1_code/zadania.py
@@ -44,3 +44,55 @@ unikalne_imiona = list({elem['name'] for elem in lista_imion})
 
 # Wypisz unikalne imiona
 print(unikalne_imiona)
+
+# ZADANIE6: Generowanie tablicy 100 pseudo-randomowych liczb
+tablica_liczb = [random.randint(1, 1000) for _ in range(100)]
+
+# ZADANIE7: Implementacja sortowania bąbelkowego
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Sortowanie tablicy z ZADANIA6
+bubble_sort(tablica_liczb)
+
+# ZADANIE8: Sortowanie setu z ZADANIA4 za pomocą sortowania bąbelkowego
+set_do_posortowania = {elem['name'] for elem in lista_imion if elem['type'] == typ_imienia_do_wyszukania}
+set_posortowany = list(set_do_posortowania)
+bubble_sort(set_posortowany)
+
+# ZADANIE9: Wyszukiwanie imienia 'JULIAN' w posortowanej tablicy z ZADANIA7
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == item:
+            return f"Imię {item} zostało znalezione na pozycji {mid}."
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return f"Imię {item} nie zostało znalezione."
+
+# Wyszukiwanie imienia 'JULIAN' w posortowanej tablicy
+wynik_wyszukiwania = binary_search(tablica_liczb, 500)  # Wyszukiwanie przykładowej liczby 500
+
+# ZADANIE10: Pomiar czasu sortowania i wyszukiwania
+start_time_sortowania = time.time()
+bubble_sort(tablica_liczb)
+end_time_sortowania = time.time()
+czas_sortowania = end_time_sortowania - start_time_sortowania
+
+start_time_wyszukiwania = time.time()
+wynik_wyszukiwania = binary_search(tablica_liczb, 500)  # Wyszukiwanie przykładowej liczby 500
+end_time_wyszukiwania = time.time()
+czas_wyszukiwania = end_time_wyszukiwania - start_time_wyszukiwania
+
+# Wyświetlenie czasu sortowania i wyszukiwania
+print("Czas sortowania:", czas_sortowania, "sekundy")
+print("Czas wyszukiwania:", czas_wyszukiwania, "sekundy")
