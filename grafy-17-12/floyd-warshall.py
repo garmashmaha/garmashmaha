1816f1ab19323d184b03bac1b1e06741a380043d
Maria <73954652+garmashmaha@users.noreply.github.com>
2023-12-17 15:51:05 +0000

    wierzcholki update

--- grafy-17-12/floyd-warshall.py
+++ grafy-17-12/floyd-warshall.py
@@ -1,13 +1,13 @@
 def floyd_warshall(graph):
     # Inicjalizujemy macierz odległości jako macierz wag krawędzi w grafie.
-    distance = graph.copy()
+    distance = {v: dict(graph[v]) for v in graph}
 
-    num_vertices = len(graph)
+    vertices = list(graph.keys())
 
     # Przechodzimy przez wszystkie wierzchołki i aktualizujemy macierz odległości.
-    for k in range(num_vertices):
-        for i in range(num_vertices):
-            for j in range(num_vertices):
+    for k in vertices:
+        for i in vertices:
+            for j in vertices:
                 # Jeśli odległość do wierzchołka j przez wierzchołek k jest mniejsza niż dotychczas znana
                 # najkrótsza odległość do j, aktualizujemy najkrótszą odległość.
                 if distance[i][k] + distance[k][j] < distance[i][j]:
@@ -17,21 +17,20 @@ def floyd_warshall(graph):
 
 def create_graph():
     num_vertices = int(input("Podaj liczbę wierzchołków: "))
-    graph = [[float('infinity') for _ in range(num_vertices)] for _ in range(num_vertices)]
+    graph = {}
 
-    # Ustawiamy wartości na przekątnej na 0, ponieważ odległość od wierzchołka do samego siebie wynosi 0.
-    for i in range(num_vertices):
-        graph[i][i] = 0
+    for _ in range(num_vertices):
+        vertex_name = input("Podaj nazwę wierzchołka: ")
+        graph[vertex_name] = {}
 
-    for i in range(num_vertices):
-        num_edges = int(input(f"Podaj liczbę krawędzi z wierzchołka {i+1}: "))
+        num_edges = int(input(f"Podaj liczbę krawędzi z wierzchołka {vertex_name}: "))
 
-        for j in range(num_edges):
-            edge_index = int(input(f"Podaj indeks wierzchołka docelowego dla krawędzi {j+1} z wierzchołka {i+1}: ")) - 1
-            edge_weight = int(input(f"Podaj wagę dla krawędzi {j+1} z wierzchołka {i+1} do wierzchołka {edge_index+2}: "))
+        for _ in range(num_edges):
+            edge_name = input(f"Podaj nazwę wierzchołka docelowego dla krawędzi z wierzchołka {vertex_name}: ")
+            edge_weight = int(input(f"Podaj wagę dla krawędzi z wierzchołka {vertex_name} do wierzchołka {edge_name}: "))
 
             # Dodajemy krawędź do grafu.
-            graph[i][edge_index] = edge_weight
+            graph[vertex_name][edge_name] = edge_weight
 
     # Zwracamy utworzony graf.
     return graph
